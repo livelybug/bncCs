19e84e2436a54482d27886c06677075bd2870c3a
livelybug <livelybug@gmail.com>
2021-07-24 17:08:48 +0800

    release for appchain registration

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -255,7 +255,7 @@ fn testnet_genesis(
 		},
 		octopus_appchain: OctopusAppchainConfig {
 			appchain_id: "".to_string(),
-			relay_contract: "octopus-relay.testnet".to_string(),
+			relay_contract: "dev-oct-relay.testnet".to_string(),
 			validators: initial_authorities.iter().map(|x| (x.0.clone(), x.6)).collect(),
 			asset_id_by_name: vec![("usdc.testnet".to_string(), 0)],
 		},
